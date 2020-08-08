b801d6af0cfbdb8b5bc967ecb618e7337504c049
baymax44 <smartbay2021@outlook.com>
2020-08-07 23:07:19 -0400

    add bloglist comp

---
+++ src/BlogList.js
@@ -0,0 +1,15 @@
+const BlogList = ({ blogs, title }) => {
+  return (
+    <div className="blog-list">
+      <h1>{ title }</h1>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+ 
+export default BlogList;
