b36b08f3c20234ddce8826166293ecfe1f6f8d99
baymax44 <smartbay2021@outlook.com>
2020-08-10 01:24:37 -0400

    reusing component

--- src/Home.js
+++ src/Home.js
@@ -11,6 +11,7 @@ const Home = () => {
   return (
     <div className="home">
       <BlogList blogs={blogs} title="All blogs" />
+      <BlogList blogs={blogs.filter((blog) => blog.author === 'mario')} title="Mario's blogs" />
     </div>
   );
 }
