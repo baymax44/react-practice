14bad5629ac42fbe68ddaeb66c1441dd9291b0aa
baymax44 <smartbay2021@outlook.com>
2020-08-14 01:45:43 -0400

    loader

--- src/Home.js
+++ src/Home.js
@@ -3,19 +3,22 @@ import BlogList from './BlogList'
 
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
 
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then(res => {
-        return res.json()
+        return res.json();
       })
       .then(data => {
-        setBlogs(data)
+        setBlogs(data);
+        setIsPending(false);
       })
   }, [])
 
   return (
     <div className="home">
+      {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All blogs" />}
     </div>
   );
