990944c4fbad24c63ade30c4de4af99be16d141c
baymax44 <smartbay2021@outlook.com>
2020-08-25 11:46:05 -0400

    route params

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,15 @@
+import { Link } from "react-router-dom";
+
 const BlogList = ({ blogs, title }) => {
   return (
     <div className="blog-list">
       <h1>{ title }</h1>
       {blogs.map((blog) => (
         <div className="blog-preview" key={blog.id}>
-          <h2>{ blog.title }</h2>
-          <p>Written by { blog.author }</p>
+          <Link to={`/blogs/${blog.id}`}>
+            <h2>{ blog.title }</h2>
+            <p>Written by { blog.author }</p>
+          </Link>
         </div>
       ))}
     </div>
