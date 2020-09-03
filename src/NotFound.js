d7175a4725607e70fe1b4a9a6449d76367a4bcab
baymax44 <smartbay2021@outlook.com>
2020-09-03 12:12:41 -0400

    add 404

---
+++ src/NotFound.js
@@ -0,0 +1,11 @@
+const NotFound = () => {
+  return (
+    <div className="not-found">
+      <h2>Sorry</h2>
+      <p>That page cannot be found</p>
+      <Link to="/">Back to the homepage...</Link>
+    </div>
+  );
+}
+ 
+export default NotFound;
